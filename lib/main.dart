429d116cbcba03c0e1fe5199fb71afd71d6138fc
ucdoganaksesuarlary <ucdogan.com@gmail.com>
2026-09-04 12:10:47 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,14 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const TkmApp());
 }
 
